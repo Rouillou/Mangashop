69035ecde390bf2151b7ecd4cb2f0e43ed161139
Rouillou <clemvarin081004@gmail.com>
2023-05-10 13:34:11 +0200

    stockage des item

--- app/Http/Controllers/MangaController.php
+++ app/Http/Controllers/MangaController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Resources\MangaResource;
+use App\Models\Manga;
 
 class MangaController extends Controller
 {
